ee78b7bb15ae13922ab8244bd8d98fc3c61735a7
pengxiaoting <564862350@qq.com>
2016-05-28 14:06:13 +0800

    datadict修改
    Signed-off-by: pengxiaoting <564862350@qq.com>

--- Xiaomi/src/main/java/com/xiaomi/web/action/DatadictAction.java
+++ Xiaomi/src/main/java/com/xiaomi/web/action/DatadictAction.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import com.opensymphony.xwork2.ModelDriven;
 import com.xiaomi.entity.Datadict;
 import com.xiaomi.entity.JsonObject;
-import com.xiaomi.entity.Manager;
 import com.xiaomi.service.DatadictService;
 
 @Controller("datadictAction")
@@ -24,9 +23,11 @@ public class DatadictAction implements ModelDriven<Datadict>,SessionAware {
 	private JsonObject<Datadict> jsonObject;
 	private int page;
 	private int rows;
+	private String dirIds;
 
-	public void setJsonObject(JsonObject<Datadict> jsonObject) {
-		this.jsonObject = jsonObject;
+
+	public JsonObject<Datadict> getJsonObject() {
+		return jsonObject;
 	}
 
 	public void setPage(int page) {
@@ -37,12 +38,24 @@ public class DatadictAction implements ModelDriven<Datadict>,SessionAware {
 		this.rows = rows;
 	}
 	
+	public void setDirIds(String dirIds) {
+		this.dirIds = dirIds;
+	}
+
+	//分页获取所有数据字典信息的方法
 	public String getAllDatadict(){
 		List<Datadict> datadicts = datadictService.find(page, rows);
 		int total = datadictService.total();
 		jsonObject=new JsonObject<Datadict>();
 		jsonObject.setRows(datadicts);
 		jsonObject.setTotal(total);
+		return "success";
+	}
+	
+	public String addDatadict(){
+		int result = datadictService.addDatadict(datadict);
+		jsonObject = new JsonObject<Datadict>();
+		jsonObject.setTotal(result);
 		return "success";
 	}
 
